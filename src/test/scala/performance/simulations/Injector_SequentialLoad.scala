6a60accf649b2a3862e8353edb03906001af601e
Tarun Kale <tkale@salesforce.com>
2018-04-26 12:01:30 -0700

    Enable CreateStory Tests alone.

--- src/test/scala/performance/simulations/Injector_SequentialLoad.scala
+++ src/test/scala/performance/simulations/Injector_SequentialLoad.scala
@@ -36,15 +36,15 @@ class Injector_SequentialLoad extends Simulation {
     //postRequest.scnIsPersonActive.inject( nothingFor((steadyTime+rampTime+sleepTime) seconds), rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
     //postRequest.scnListCauses.inject( nothingFor(((steadyTime+rampTime)*1)+sleepTime seconds), rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
     //postRequest.scnListSDG.inject( nothingFor(((steadyTime+rampTime)*2)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
-    postRequest.scnCreateStory.inject( nothingFor(((steadyTime+rampTime)*1)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
+    postRequest.scnCreateStory.inject( rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST)
     //postRequest.scnGetStory.inject( nothingFor(((steadyTime+rampTime)*4)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
     //postRequest.scnCreateCampaign.inject( nothingFor(((steadyTime+rampTime)*5)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
     //postRequest.scnGetCampaign.inject( nothingFor(((steadyTime+rampTime)*6)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
     //postRequest.scnCreateImpactFund.inject( nothingFor(((steadyTime+rampTime)*7)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
     //postRequest.scnGetImpactFund.inject( nothingFor(((steadyTime+rampTime)*8)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
     //postRequest.scnGetOrganization.inject( nothingFor(((steadyTime+rampTime)*9)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
-    postRequest.scnUpdatePrefCauses.inject( nothingFor(((steadyTime+rampTime)*2)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
-    postRequest.scnHomePageSettings.inject( nothingFor(((steadyTime+rampTime)*3)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST)
+    //postRequest.scnUpdatePrefCauses.inject( nothingFor(((steadyTime+rampTime)*2)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST),
+    //postRequest.scnHomePageSettings.inject( nothingFor(((steadyTime+rampTime)*3)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST)
     //postRequest.scnBrandSettings.inject( nothingFor(((steadyTime+rampTime)*12)+sleepTime seconds),rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST)
     //postRequest.scnCreateNpoPage.inject( rampUsersPerSec(1) to (peakRPS_SequentialLoad) during (rampTime seconds), constantUsersPerSec(peakRPS_SequentialLoad) during(steadyTime seconds)).protocols(httpTEST)
 
@@ -59,7 +59,7 @@ class Injector_SequentialLoad extends Simulation {
     //details(postRequest.grpSDG / "ListSDG" ).responseTime.mean.lte( meanResponseTime),
     //details(postRequest.grpSDG / "ListSDG" ).failedRequests.percent.lte( errorRate),
     details(postRequest.grpStory / "CreateStory" ).responseTime.mean.lte( meanResponseTime),
-    details(postRequest.grpStory / "CreateStory" ).failedRequests.percent.lte( errorRate),
+    details(postRequest.grpStory / "CreateStory" ).failedRequests.percent.lte( errorRate)
     //details(postRequest.grpStory / "GetStory" ).responseTime.mean.lte( meanResponseTime),
     //details(postRequest.grpStory / "GetStory" ).failedRequests.percent.lte( errorRate),
     //details(postRequest.grpCampaign / "CreateCampaign" ).responseTime.mean.lte( meanResponseTime),
@@ -72,10 +72,10 @@ class Injector_SequentialLoad extends Simulation {
     //details(postRequest.grpImpactFund / "GetImpactFund" ).failedRequests.percent.lte( errorRate),
     //details(postRequest.grpOrganization / "GetOrganization" ).responseTime.mean.lte( meanResponseTime),
     //details(postRequest.grpOrganization / "GetOrganization" ).failedRequests.percent.lte( errorRate),
-    details(postRequest.grpCauses / "UpdatePrefCauses" ).responseTime.mean.lte( meanResponseTime),
-    details(postRequest.grpCauses / "UpdatePrefCauses" ).failedRequests.percent.lte( errorRate),
-    details(postRequest.grpSettings / "HomePageSettings" ).responseTime.mean.lte( meanResponseTime),
-    details(postRequest.grpSettings / "HomePageSettings" ).failedRequests.percent.lte( errorRate)
+    //details(postRequest.grpCauses / "UpdatePrefCauses" ).responseTime.mean.lte( meanResponseTime),
+    //details(postRequest.grpCauses / "UpdatePrefCauses" ).failedRequests.percent.lte( errorRate),
+    //details(postRequest.grpSettings / "HomePageSettings" ).responseTime.mean.lte( meanResponseTime),
+    //details(postRequest.grpSettings / "HomePageSettings" ).failedRequests.percent.lte( errorRate)
     //details(postRequest.grpSettings / "BrandSettings").responseTime.mean.lte( meanResponseTime),
     //details(postRequest.grpSettings / "BrandSettings").failedRequests.percent.lte( errorRate)
     //details(postRequest.grpNpoPage / "CreateNpoPage" ).responseTime.mean.lte( meanResponseTime),
