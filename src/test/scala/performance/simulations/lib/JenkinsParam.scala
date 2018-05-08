1961507533d62105afbf0b1a2bcdf0d485c3ebbb
Tarun Kale <tkale@salesforce.com>
2018-05-08 12:50:20 -0700

    Updated Token and load pattern

--- src/test/scala/performance/simulations/lib/JenkinsParam.scala
+++ src/test/scala/performance/simulations/lib/JenkinsParam.scala
@@ -9,11 +9,11 @@ import java.lang._
 
 object JenkinsParam {
 
-  //val TEST_URL : String = sys.env.getOrElse("TEST_URL","https://ngp-perf-zuulapp.makanaplatform.com")
-  val TEST_URL : String = sys.env.getOrElse("TEST_URL","http://ngp-perf-incaapp.herokuapp.com")
+  val TEST_URL : String = sys.env.getOrElse("TEST_URL","https://ngp-perf-zuulapp.makanaplatform.com")
+  //val TEST_URL : String = sys.env.getOrElse("TEST_URL","http://ngp-perf-incaapp.herokuapp.com")
   val TEST_URL_SANDBOX_GREEN : String = sys.env.getOrElse("TEST_URL_SANDBOX_GREEN","http://sandboxincaapp.herokuapp.com/")
 
-  val peakRPS : Double =  Double.parseDouble( getSetting("peakRPS","2"))
+  val peakRPS : Double =  Double.parseDouble( getSetting("peakRPS","4"))
   val peakRPS_SequentialLoad : Double =  Double.parseDouble( getSetting("peakRPS_SequentialLoad","40"))
   val peakRPS_SequentialLoad_LowRPS : Double =  Double.parseDouble( getSetting("peakRPS_SequentialLoad_LowRPS","20"))
   val peakRPS_SandboxGreen : Double =  Double.parseDouble( getSetting("peakRPS_SandboxGreen","5"))
